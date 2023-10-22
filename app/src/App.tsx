5a2e5b9ade561cf173d62304c09be7b05b0712ee
manTo16 <fmantov5@gmail.com>
2023-10-22 20:55:26 +0200

    starting navbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Prova</h1>
       <p>sbruffa</p>
     </div>
---
+++ app/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavbarBootstrap from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Navbar() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Navbar;
