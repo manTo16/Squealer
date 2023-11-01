f49f86d40b7e93344b0c3b0476805f24145bee72
manTo16 <fmantov5@gmail.com>
2023-11-01 15:38:12 +0100

    gggg

--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav'
+
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div>
+      <Nav className='sidebar d-flex flex-column'>
+        <Nav.Item>
+          Channel1
+        </Nav.Item>
+        <Nav.Item>
+          Channel2
+        </Nav.Item>
+        <Nav.Item>
+          Channel3
+        </Nav.Item>
+        <Nav.Item>
+          Channel4
+        </Nav.Item>
+      </Nav>
+    </div>
   )
 }
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/col'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
     <div>
       <Container className='h-100'>
         <Row>
-          <Col></Col>
-          <Col xs={6} className='bg-dark'></Col>
+          <Col xs={2} className='sidebar-wrapper'>
+            <Sidebar />
+          </Col>
+          <Col xs={6} className='content-wrapper'></Col>
+          <Col xs={4} className=''></Col>
           <Col></Col>
         </Row>
       </Container>
