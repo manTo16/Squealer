c3c32cccd2161f396c3496d9376433e94e7c4d6f
Alberto Zuccari <albertozuccari8@gmail.com>
2023-11-29 13:43:36 +0100

    un cazzo proprio

--- app/src/components/Bottombar.tsx
+++ app/src/components/Bottombar.tsx
@@ -6,11 +6,11 @@ import Button from 'react-bootstrap/Button';
 import "./Bottombar.css"
 
 import Logo from "../assets/Squealer.png"
-import HomeButton from './Button/HomeButton';
-import FireButton from './Button/FireButton';
-import AddButton from './Button/AddButton';
-import QuestionButton from './Button/QuestionButton';
-import GroupButton from './Button/GroupButton';
+import HomeButton from './Button/BottomBarButtons/HomeButton';
+import FireButton from './Button/BottomBarButtons/FireButton';
+import AddButton from './Button/BottomBarButtons/AddButton';
+import QuestionButton from './Button/BottomBarButtons/QuestionButton';
+import GroupButton from './Button/BottomBarButtons/GroupButton';
 
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
@@ -20,62 +20,56 @@ import { Col } from 'react-bootstrap';
 icone:
 https://reactsvgicons.com/materialdesignicons?page=1
 */
+import "./Bottombar.css"
 
-
-function Bottombar() {
+export default function Bottombar() {
   return (
-    <>
       <Navbar fixed="bottom" bg="dark" data-bs-theme="dark">
-      <Nav className="me-auto"> 
-        <Container>
-            <Row>
+        <Nav className="d-flex justify-content-around">  {/*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*/}
+          <Container>
+              <Row>
+                <Col>
+                  <Nav.Item className='d-flex justify-content-around'>
+                      <Button variant="outline-light">
+                        <HomeButton />
+                      </Button>
+                  </Nav.Item>
+                </Col>
+        
+                <Col>
+                  <Nav.Item className='d-flex justify-content-around'>
+                    <Button variant="outline-light">
+                      <FireButton/>
+                    </Button>
+                  </Nav.Item>
+                </Col>
 
-            <Col>
-              <Nav.Item className='d-flex justify-content-around'>
-                  <Button variant="outline-light">
-                    <HomeButton />
-                  </Button>
-              </Nav.Item>
-            </Col>
-    
-            <Col>
-              <Nav.Item className='d-flex justify-content-around'>
-                <Button variant="outline-light">
-                  <FireButton/>
-                </Button>
-              </Nav.Item>
-            </Col>
+                <Col className='justify-content-start'>
+                  <Nav.Item className='d-flex justify-content-around'>
+                    <Button variant="outline-light" href="/newPost">
+                      <AddButton />
+                    </Button>
+                  </Nav.Item>
+                </Col>
 
-            <Col className='justify-content-start'>
-              <Nav.Item className='d-flex justify-content-around'>
-                <Button variant="outline-light" href="/newPost">
-                  <AddButton />
-                </Button>
-              </Nav.Item>
-            </Col>
+                <Col>
+                  <Nav.Item className='d-flex justify-content-around'>
+                    <Button variant="outline-light">
+                      <QuestionButton/>
+                    </Button>
+                  </Nav.Item>
+                </Col>
 
-            <Col>
-              <Nav.Item className='d-flex justify-content-around'>
-                <Button variant="outline-light">
-                  <QuestionButton/>
-                </Button>
-              </Nav.Item>
-            </Col>
-
-            <Col>
-              <Nav.Item className='d-flex justify-content-around'>
-                <Button variant="outline-light">
-                  <GroupButton/> 
-                </Button>
-              </Nav.Item>
-            </Col>
-
-            </Row>
-        </Container>
-      </Nav>
+                <Col>
+                  <Nav.Item className='d-flex justify-content-around'>
+                    <Button variant="outline-light">
+                      <GroupButton/> 
+                    </Button>
+                  </Nav.Item>
+                </Col>
+              </Row>
+          </Container>
+        </Nav>
       </Navbar>
-    </>
   );
 }
-
-export default Bottombar;
