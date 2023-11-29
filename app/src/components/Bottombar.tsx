e0d08500c7190fc986c2815ec64c7985cbf7ad99
IHateLiam <liambusnelli02@gmail.com>
2023-11-29 13:10:41 +0100

    indentazione

--- app/src/components/Bottombar.tsx
+++ app/src/components/Bottombar.tsx
@@ -67,7 +67,7 @@ function Bottombar() {
                   fill="currentColor"
                   height="1em"
                   width="1em"
-                >
+                  >
                   <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z" />
                 </svg>
                 </Button>
