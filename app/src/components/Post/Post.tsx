19faecf65c77a2ce91f1a3d3f488a5a9f61ec163
IHateLiam <liambusnelli02@gmail.com>
2023-12-24 21:57:42 +0100

    dati post un pelo più dinamici

--- app/src/components/Post/Post.tsx
+++ app/src/components/Post/Post.tsx
@@ -4,7 +4,44 @@ import propic from "../../assets/person/9.png"
 import Like from "../svg/Reaction/LikeSvg"
 import Heart from "../svg/Reaction/HeartSvg"
 
+import { MutableRefObject, useRef } from "react"
+
+function loadPostData(
+    postText: MutableRefObject<string>,
+    postDisplayedName: MutableRefObject<string>,
+    postUsername: MutableRefObject<string>,
+    postLikeCounter: MutableRefObject<number>,
+    postCommentsCounter: MutableRefObject<number>
+    ) {
+    /* 
+    qua probabilmente metteremo delle chiamate al database per popolare gli elementi del post
+    idealmente l'unico argomento di loadPostData è un id che identifica il post,
+        perchè mettere tanti argomenti mi sembra bruttino, ma va poi bene uguale.
+        per ora dobbiamo tenerci tanti argomenti perchè non so come gestire lo scope della funzione altrimenti
+    mancano delle cose, tipo l'immagine
+    */
+
+   //elemento.current = api get post_id o qualcosa del genere
+    postText.current = "Ei fu. Siccome immobile, Dato il mortal sospiro, Stette la spoglia immemore Orba di tanto spiro, Così percossa, attonita La terra al nunzio sta, Muta pensando all’ultima Ora dell’uom fatale; Nè sa quando una simile Orma di piè mortale La sua cruenta polvere A calpestar verrà."
+    postDisplayedName.current = "Tova";
+    postUsername.current = "@Phepega69";
+    postLikeCounter.current = 69;
+    postCommentsCounter.current = 9;
+}
+
 export default function Post() {
+    const postText = useRef("");
+    const postDisplayedName = useRef("");
+    const postUsername = useRef("");
+    const postLikeCounter = useRef(0);
+    const postCommentsCounter = useRef(0);
+
+    let postTextLength = 0;
+
+    loadPostData(postText, postDisplayedName, postUsername, postLikeCounter, postCommentsCounter);
+
+    postTextLength = postText.current.length;
+
     return (
         <div className="post">
             <div className="postWrapper">
@@ -15,16 +52,16 @@ export default function Post() {
                             src={propic}
                             alt="" 
                         />
-                        <span className="postDisplayedName">Tova</span>
-                        <span className="postUsername">@Phepega69</span>
+                        <span className="postDisplayedName">{postDisplayedName.current}</span>
+                        <span className="postUsername">{postUsername.current}</span>
                     </div>
                     <div className="postTopRight">
-                        <span className="postCharUsed">42</span>
+                        <span className="postCharUsed">{postTextLength}</span>
                         <VDots />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Ei fu. Siccome immobile, Dato il mortal sospiro, Stette la spoglia immemore Orba di tanto spiro, Così percossa, attonita La terra al nunzio sta, Muta pensando all’ultima Ora dell’uom fatale; Nè sa quando una simile Orma di piè mortale La sua cruenta polvere A calpestar verrà.</span>
+                    <span className="postText">{postText.current}</span>
                     <img className="postImg" src="/assets/post/2.jpg" alt="" />
                 </div>
                 <div className="postBottom">
@@ -35,10 +72,10 @@ export default function Post() {
                         <button className="btn-transparent">
                             <Heart className="likeIcon"/>
                         </button>
-                        <span className="postLikeCounter">piace a 69 utenti</span>
+                        <span className="postLikeCounter">piace a {postLikeCounter.current} utenti</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">9 commenti</span>
+                        <span className="postCommentText">{postCommentsCounter.current} commenti</span>
                     </div>
                 </div>
             </div>
---
+++ linkUtili.md
@@ -0,0 +1,13 @@
+# risorse
+## qua possiamo mettere link a risorse che possono essere utili, per esempio spiegazioni di buone abitudini che conviene mantenere ma che non siamo sicurissimi di ricordare magari
+
+
+### differenza tra e uso di String e string in typescript
+https://stackoverflow.com/questions/14727044/what-is-the-difference-between-types-string-and-string
+
+riassunto: usiamo **string** perchè la comunità li accetta come migliori di **String**
+
+### guida mdn su accessibilità
+https://developer.mozilla.org/en-US/docs/Learn/Accessibility/HTML
+
+non l'ho letto tutto ma sicuramente servirà. se lo leggete scrivete qua dei riassuntini
