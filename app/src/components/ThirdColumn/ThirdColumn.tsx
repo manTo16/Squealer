ac693e112e2114aa09fd9d7af3ef73e5ef695c34
IHateLiam <liambusnelli02@gmail.com>
2024-02-21 12:27:34 +0100

    corretto canale random RANDOM

--- app/src/components/ThirdColumn/ThirdColumn.tsx
+++ app/src/components/ThirdColumn/ThirdColumn.tsx
@@ -47,7 +47,7 @@ export default function ThirdColumn() {
             className="w-100 mb-1 d-flex justify-content-left align-items-center"
           >
             <QuestionButton className="mx-2"/>
-            <span className="mx-2">§Random</span>
+            <span className="mx-2">§RANDOM</span>
           </Button>
         </div>
   );
